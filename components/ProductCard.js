17f2e07f1411c7d963e1470b9aef6b88e3ecc12b
Milan Lakhani <milanlakhaniaiml@gmail.com>
2023-08-17 22:45:55 -0400

    fix: bugs with images

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -7,10 +7,15 @@ export default function ProductCard({ product }) {
   function cn(...classes) {
     return classes.filter(Boolean).join(' ')
   }
+
+  console.log("ProductCard")
+  console.log(product)
+
   return (
     <Link href={`/products/${product.id}`} className="group">
       <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-        <Image
+        {
+          false ? <Image
           alt=""
           src={product.images[0].file.url}
           fill
@@ -22,14 +27,15 @@ export default function ProductCard({ product }) {
               : 'scale-100 blur-0 grayscale-0'
           )}
           onLoadingComplete={() => setLoading(false)}
-        />
+        /> : null
+        }
       </div>
       <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
         <h3>{product.name}</h3>
         <p>${product.price}</p>
       </div>
       <p className="mt-1 text-sm italic text-gray-500">
-        {product.options[0].values[0].name} calories
+        {/* {product.options[0].values[0].name} calories */}
       </p>
     </Link>
   )
